a7cc4980c7af1ce7146254a4db6ab10ce5c087e0
MariteMarques <161392274+MariteMarques@users.noreply.github.com>
2024-05-30 14:33:50 -0600

    Delete pregunta 4 cuestionario.sql

--- pregunta 4 cuestionario.sql
+++
@@ -1,7 +0,0 @@
-#pregunta Número 4#
-
-DELETE FROM parties WHERE party_race is NULL;
-SELECT party_race, party_age, party_sex, county_location
-FROM  parties
-JOIN collisions ON collisions.case_id = parties.case_id
-GROUP BY party_race;
