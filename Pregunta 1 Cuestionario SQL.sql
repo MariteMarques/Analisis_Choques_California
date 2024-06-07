f7af1c5ee512c8b35e9dccc767c4b84aba77af3b
MariteMarques <161392274+MariteMarques@users.noreply.github.com>
2024-06-06 19:44:29 -0600

    Revert "Merge branch 'Alfonso' of https://github.com/MariteMarques/Analisis_Choques_California into Alfonso"
    This reverts commit b1b54c0d297f45537f45508e6fb7a56f4b7e83bf, reversing
    changes made to ae72997102daa8281e13f55464c596c039887f94.

--- Pregunta 1 Cuestionario SQL.sql
+++
@@ -1,5 +0,0 @@
-#pregunta Número 1#
-
-SELECT county_location AS Ciudad, COUNT(case_id) AS Casos_Totales, SUM(killed_victims) AS Victimas FROM collisions
-GROUP BY county_location
-ORDER BY Casos_Totales DESC;
